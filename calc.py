58a656ea8ae2b84613a1296e7e2794edd482f8eb
RsusMain <romanscool2000@gmail.com>
2024-06-14 23:59:46 +0300

    first commit

---
+++ calc.py
@@ -0,0 +1,11 @@
+def main():
+    x = int(input("what's x?"))
+    print(square(x))
+
+def square(n):
+
+    return n*n
+
+
+
+main()
---
+++ hello.py
@@ -0,0 +1,9 @@
+def main():
+    name = input("What's yout name? ")
+    hello(name)
+
+def hello(to="world"):
+    print("Hello,", to)
+
+
+main()
