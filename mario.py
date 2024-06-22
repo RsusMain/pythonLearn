d7fc86eaa2c084067690a412bcc36e513a7d5cfa
RsusMain <romanscool2000@gmail.com>
2024-06-22 11:17:43 +0300

    mar

--- mario.py
+++ mario.py
@@ -1,11 +1,10 @@
 def main():
-    print_column(3)
+    print_square(3)
 
 
-def print_column(height):
-    for _ in range(height):
-        print("#")
-    print("#\n" * height, end = "")
+def print_square(size):
+    for i in range(size):     
+        print("#" * size)
 
 
 main()
